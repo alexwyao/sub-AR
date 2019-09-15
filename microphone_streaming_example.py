038cb97847065887aad1830b9fc925693d87f6f4
anyu0 <wanganyu12@gmail.com>
2019-09-15 00:38:08 -0400

    update streaming with diff

--- microphone_streaming_example.py
+++ microphone_streaming_example.py
@@ -1,22 +1,14 @@
-"""Copyright 2019 Google, Modified by REV 2019
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
 import json
 import pyaudio
 from rev_ai.models import MediaConfig
 from rev_ai.streamingclient import RevAiStreamingClient
 from six.moves import queue
+
+import audioop
+
+
+def get_rms( block ):
+    return audioop.rms(block, 2)
 
 
 class MicrophoneStream(object):
@@ -26,7 +18,10 @@ class MicrophoneStream(object):
         self._chunk = chunk
         # Create a thread-safe buffer of audio data
         self._buff = queue.Queue()
+        self._buff1 = queue.Queue()
         self.closed = True
+
+        self.a_left = self.a_right = self.a_diff = 0
 
     def __enter__(self):
         self._audio_interface = pyaudio.PyAudio()
@@ -39,6 +34,19 @@ class MicrophoneStream(object):
             # This is necessary so that the input device's buffer doesn't
             # overflow while the calling thread makes network requests, etc.
             stream_callback=self._fill_buffer,
+            input_device_index=0,
+        )
+
+        self._audio_stream1 = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            channels=1, rate=self._rate,
+            input=True, frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer1,
+            input_device_index=1,
         )
 
         self.closed = False
@@ -47,16 +55,24 @@ class MicrophoneStream(object):
 
     def __exit__(self, type, value, traceback):
         self._audio_stream.stop_stream()
+        self._audio_stream1.stop_stream()
         self._audio_stream.close()
+        self._audio_stream1.close()
         self.closed = True
         # Signal the generator to terminate so that the client's
         # streaming_recognize method will not block the process termination.
         self._buff.put(None)
+        self._buff1.put(None)
         self._audio_interface.terminate()
 
     def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
         """Continuously collect data from the audio stream, into the buffer."""
         self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def _fill_buffer1(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff1.put(in_data)
         return None, pyaudio.paContinue
 
     def generator(self):
@@ -65,21 +81,32 @@ class MicrophoneStream(object):
             # data, and stop iteration if the chunk is None, indicating the
             # end of the audio stream.
             chunk = self._buff.get()
+            chunk1 = self._buff1.get()
             if chunk is None:
                 return
             data = [chunk]
+            data1 = [chunk1]
 
             # Now consume whatever other data's still buffered.
             while True:
                 try:
                     chunk = self._buff.get(block=False)
+                    chunk1 = self._buff1.get(block=False)
                     if chunk is None:
                         return
                     data.append(chunk)
+                    data1.append(chunk1)
                 except queue.Empty:
                     break
 
-            yield b''.join(data)
+            block = b''.join(data)
+            block1 = b''.join(data1)
+            amplitude = get_rms( block )
+            amplitude1 = get_rms( block1 )
+            self.a_diff = amplitude - amplitude1
+            # print(amplitude,amplitude1,amplitude - amplitude1)
+
+            yield block
 
 
 # Sampling rate of your microphone and desired chunk size
@@ -105,6 +132,7 @@ with MicrophoneStream(rate, chunk) as stream:
         for response in response_gen:
             try:
                 print( [a["value"] for a in json.loads(response)["elements"]])
+                print( stream.a_diff)
             except:
                 print("NOOOO")
 
